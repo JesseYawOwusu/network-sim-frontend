3de26e26d8d09e7d65e7624b165a10b2e3b8baa3
selinaKudadze <kudadzeselina@gmail.com>
2025-10-17 13:12:21 +0000

    Fixed form submission issue by removing the click event in button attribiutes and changing button type to submit

--- src/app/authentication/signup/signup.ts
+++ src/app/authentication/signup/signup.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { Component,OnInit, inject} from '@angular/core';
+import {RouterLink,Router} from "@angular/router";
+import {ReactiveFormsModule,FormControl,FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
-export class Signup {
+export class Signup implements OnInit {
+  private router=inject(Router);
+
+  signupForm:FormGroup=new FormGroup({
+      username:new FormControl(''),
+      email:new FormControl(''),
+      password:new FormControl(''),
+      confirmPassword:new FormControl('')
+  });
+
+  constructor(){}
+
+  ngOnInit(){
+    this.signupForm=new FormGroup({
+      username:new FormControl(''),
+      email:new FormControl(''),
+      password:new FormControl(''),
+      confirmPassword:new FormControl('')
+    })
+  }
+
+  confirmPassword(){
+    
+  }
+
+  submitSignup(){
+    console.log(this.signupForm.value);
+  }
+  
+  afterSubmit(){
+    this.router.navigate(['/login'])
+
+  }
+  
+   
+
 
 }
