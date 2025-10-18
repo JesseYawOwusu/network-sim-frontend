47b532265717910c7dc995f61d6a2f21c1f2b55a
selinaKudadze <kudadzeselina@gmail.com>
2025-10-18 10:39:59 +0000

    removed directives folder

--- src/app/directives/confirm-password.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConfirmPassword } from './confirm-password';
-
-describe('ConfirmPassword', () => {
-  it('should create an instance', () => {
-    const directive = new ConfirmPassword();
-    expect(directive).toBeTruthy();
-  });
-});
