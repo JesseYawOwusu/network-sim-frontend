3b21a7993fedb30ee3201e97ea80da5ebb8d4411
selinaKudadze <kudadzeselina@gmail.com>
2025-10-18 10:33:08 +0000

    Created directives folder and confirmpassword directive

---
+++ src/app/directives/confirm-password.spec.ts
@@ -0,0 +1,8 @@
+import { ConfirmPassword } from './confirm-password';
+
+describe('ConfirmPassword', () => {
+  it('should create an instance', () => {
+    const directive = new ConfirmPassword();
+    expect(directive).toBeTruthy();
+  });
+});
