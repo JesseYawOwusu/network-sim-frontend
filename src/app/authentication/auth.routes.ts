69e19ebd898982f0350c120ebc4c3de3ab98734c
selinaKudadze <kudadzeselina@gmail.com>
2025-10-19 11:53:46 +0000

    Changed eagerly loaded components to lazily loaded components

--- src/app/authentication/auth.routes.ts
+++ src/app/authentication/auth.routes.ts
@@ -4,7 +4,7 @@ import {Signup} from "./signup/signup";
 
 export const authRoutes:Routes=[
     {path:'',redirectTo:'login',pathMatch:'full'},
-    {path:'login',component:Login},
-    {path:'signup',component:Signup}
+    {path:'login',loadComponent: ()=>import('./login/login').then(m=>m.Login)},
+    {path:'signup',loadComponent:()=>import('./signup/signup').then(m=>m.Signup)}
 ]
 
