5c29dfabad3f4778af3044330a7102563e6ea213
selinaKudadze <kudadzeselina@gmail.com>
2025-10-19 12:19:46 +0000

    moved network image to assets and changed route to authentication pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { Routes } from '@angular/router';
 import {authRoutes} from './authentication/auth.routes';
 
 export const routes: Routes = [
-    {path:'', children:authRoutes},
+    {path:'auth', children:authRoutes},
 ];
