b3c02a923b31de8318c007284c318c3f51c76dbc
selinaKudadze <kudadzeselina@gmail.com>
2025-10-20 15:24:31 +0000

    Used the authservice to handle loggedin users

--- src/app/authentication/login/login.ts
+++ src/app/authentication/login/login.ts
@@ -2,6 +2,7 @@ import { Component ,OnInit, ViewChild, inject,AfterViewInit} from '@angular/core
 import {RouterLink,Router} from "@angular/router";
 import {ReactiveFormsModule,FormControl,FormGroup,Validators} from "@angular/forms";
 import { InputComponent } from '../input-component/input-component';
+import {AuthServices} from '../../services/authService/authService';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,8 @@ import { InputComponent } from '../input-component/input-component';
 })
 export class Login implements OnInit {
   private router=inject(Router);
+  private authService=inject(AuthServices)
+
   public loginForm!: FormGroup<{ username: FormControl<string | null>; password: FormControl<string | null>; }>;
 
   ngOnInit(){
@@ -22,15 +25,21 @@ export class Login implements OnInit {
 
   public submitLogin(){
     if(this.loginForm.valid){
-     
-      this.afterSubmit();
+      this.authService.login(this.loginForm.controls.username.value??'',this.loginForm.controls.password.value??'')
+      .subscribe({
+        next:()=> {
+          this.router.navigate(['/auth/signup'])
+          console.log('User logged in successfully')
+        },
+        error:(err)=>console.log('We received this login error: ',err),
+        complete:()=>console.log('no errors, we are done logging in the user')
+      }
+        
+      )
   }else{
    
     this.loginForm.markAllAsTouched(); 
   }
 }
-  
-  public afterSubmit(){
-    this.router.navigate(['/auth/signup'])
-  }
+
 }
