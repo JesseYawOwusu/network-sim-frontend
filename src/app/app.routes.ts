7daed7cdaa70565127cdf3088878af4054fcac31
44 <6>
2025-10-21 16:21:36 +0000

    Fix routing to show scenario editor as default page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/scenario-editor',
+    pathMatch: 'full'
+  },
+  {
     path: 'scenario-editor',
     loadComponent: () => import('./components/scenario-editor/scenario-editor.component').then(m => m.ScenarioEditorComponent)
   }
