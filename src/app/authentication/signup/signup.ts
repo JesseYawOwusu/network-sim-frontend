91d9271dd55183736f276c0deaf979d0d5e7b2bb
selinaKudadze <kudadzeselina@gmail.com>
2025-10-21 08:06:47 +0000

    Included email regex pattern validation

--- src/app/authentication/signup/signup.ts
+++ src/app/authentication/signup/signup.ts
@@ -23,7 +23,7 @@ export class Signup implements OnInit {
   ngOnInit(){
     this.signupForm=new FormGroup({
       username:new FormControl('',[Validators.required]),
-      email:new FormControl('',[Validators.required,Validators.email]),
+      email:new FormControl('',[Validators.required,Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
       password:new FormControl('',[Validators.required]),
       confirmPassword:new FormControl('',[Validators.required]),
       role:new FormControl('',[Validators.required])
