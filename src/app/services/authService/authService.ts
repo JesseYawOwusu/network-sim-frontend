73c5e56ea3c7b3760d37ff6996ffb564a8f64f67
selinaKudadze <kudadzeselina@gmail.com>
2025-10-21 14:50:22 +0000

    Implemented unsubscription using ngOnDestroy

--- src/app/services/authService/authService.ts
+++ src/app/services/authService/authService.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
+import {OnDestroy} from '@angular/core';
 import moment from "moment";
 import {HttpClient,HttpErrorResponse} from '@angular/common/http';
-import { Observable,tap,throwError } from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import {Subject,tap } from 'rxjs';
+import {catchError, takeUntil} from 'rxjs/operators';
 import {User} from '../../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServices {
+export class AuthServices implements OnDestroy{
   private API_URL='https://genethliacally-ling-epeirogenic.ngrok-free.dev/api/authentication/';
- 
+  private _destroy$=new Subject<void>();
  
 
   constructor(private http:HttpClient){}
+
+  ngOnDestroy(){
+    this._destroy$.next();
+  }
+
   
 
   public login(email:string,password:string){
     return this.http.post(`${this.API_URL}signin`,{email,password})
     .pipe(
       tap(()=>this.setLoggedInUser), 
+      takeUntil(this._destroy$)
     )
       
   }
