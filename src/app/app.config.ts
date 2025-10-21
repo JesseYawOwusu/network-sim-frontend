0ebc50bf198e0971d3e94a15f77603cacf952c7f
selinaKudadze <kudadzeselina@gmail.com>
2025-10-21 10:58:23 +0000

    Included sweetalert2 in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {provideHttpClient} from '@angular/common/http'
+import {provideHttpClient} from '@angular/common/http';
+import {provideSweetAlert2}  from "@sweetalert2/ngx-sweetalert2"
 
 import { routes } from './app.routes';
 
@@ -10,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideSweetAlert2()
 
   ]
 };
